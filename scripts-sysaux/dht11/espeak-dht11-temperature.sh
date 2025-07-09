e201e1a18ec0ee295c304e11247399f960061f17
0x07cb <83157348+0x07CB@users.noreply.github.com>
2025-07-09 21:46:22 +0200

    Add scripts for DHT11 humidity and temperature readings with audio output

---
+++ scripts-sysaux/dht11/espeak-dht11-temperature.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+function tell_temperature(){
+    # Use the dht.py script to get the temperature and speak it using espeak-ng
+    # The output is piped to play for audio playback
+    # Custom units can be specified for better understanding
+    # Redirect stderr to /dev/null to suppress error messages
+    # The output is spoken in French with a specific speed and volume
+    # The output is played through the default audio device
+    # The script uses the --use-stdout option to output the temperature directly to stdout
+    # The custom units for humidity and temperature are specified for clarity
+
+  dht11.py --temperature --prefix-temperature="La température est de" --custom-celsius-unit="Degrés Celsius" --use-stdout 2>/dev/null | espeak-ng -v fr -a 100 -s 175 --stdout | play -
+} 
+
+
+tell_temperature
