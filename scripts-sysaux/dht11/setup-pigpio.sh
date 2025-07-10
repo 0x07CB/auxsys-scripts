fcba271c917c6830a48e9ec65061973f71041b0b
0x07cb <83157348+0x07CB@users.noreply.github.com>
2025-07-10 19:06:07 +0200

    Refactor root check section in setup-pigpio.sh for clarity and consistency

--- scripts-sysaux/dht11/setup-pigpio.sh
+++ scripts-sysaux/dht11/setup-pigpio.sh
@@ -25,9 +25,39 @@ function echo_info_message_with_ansi_colors() {
 }
 
 
-
+# START OF THE SCRIPT
+# FIRST CHECK IF THE SCRIPT IS RUN AS ROOT
+# ############################
 
 # check if this script is run as root, if not, exit with an error message
 if [ "$(id -u)" -ne 0 ]; then
     # error messages an blinking red "ERROR" between whites brackets (ANSI COLORS)
     # info messages are in yellow, and success messages are in green
+    ERROR_MESSAGE="This script must be run as root. Please use sudo or run as root."
+    # use 'echo_error_message_with_ansi_colors' function to print the error message, the function will return 1 if the message is empty (failure) and 0 if the message is printed (success).
+    # So please check and manage the return code of the function
+    echo_error_message_with_ansi_colors "$ERROR_MESSAGE"
+    if [ $? -ne 0 ]; then
+        echo "Failed to print error message. Exiting."
+        exit 1
+    fi
+    
+    # exit with a non-zero code to indicate failure ( non-root user )
+    exit 1
+fi
+
+
+
+# ############################
+
+
+# ############################
+
+# ############################
+
+# ############################
+
+
+
+
+# ############################
