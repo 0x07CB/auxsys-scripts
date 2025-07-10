0af99b71718bd258192916beffaae6de8d070385
0x07cb <83157348+0x07CB@users.noreply.github.com>
2025-07-10 20:00:04 +0200

    Amélioration des messages d'erreur et d'information avec des descriptions plus claires dans setup-pigpio.sh

--- scripts-sysaux/dht11/setup-pigpio.sh
+++ scripts-sysaux/dht11/setup-pigpio.sh
@@ -6,9 +6,8 @@ function echo_error_message_with_ansi_colors() {
         echo "No message provided to echo_error_message_with_ansi_colors function."
         return 1
     fi
-    # This function take a env var in argument ( a text string , this is the message to print )
-    # This function prints an error message in red with blinking effect
-    echo -e "\033[31;1m[\033[0m\033[31;5mERROR\033[0m\033[31;1m]\033[0m $1"
+    # This function prints an error message: brackets in white, 'ERROR' in blinking red
+    echo -e "\033[37;1m[\033[0m\033[31;5mERROR\033[0m\033[37;1m]\033[0m $1"
     return 0
 }
 
@@ -18,9 +17,8 @@ function echo_info_message_with_ansi_colors() {
         echo_error_message_with_ansi_colors "No message provided to echo_info_message_with_ansi_colors function."
         return 1
     fi
-    # This function take a env var in argument ( a text string , this is the message to print )
-    # This function prints an info message in yellow
-    echo -e "\033[33;1m[\033[0m\033[33;1mINFO\033[0m\033[33;1m]\033[0m $1"
+    # This function prints an info message: brackets in white, 'INFO' in yellow
+    echo -e "\033[37;1m[\033[0m\033[33;1mINFO\033[0m\033[37;1m]\033[0m $1"
     return 0
 }
 
